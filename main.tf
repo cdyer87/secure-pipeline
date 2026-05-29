ae9f018c839ee1f5bf8cd7d566f19f2bd3fd804d
Christopher <cdyer32@wgu.edu>
2026-05-29 17:48:57 +0000

    fix: remove simulated credentials to restore pipeline health

--- main.tf
+++ main.tf
@@ -9,8 +9,6 @@ terraform {
 
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIAIOSFODNN7EXAMPLE"
-  secret_key = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
 }
 
 # 1. Random ID Generator (S3 bucket names must be globally unique across all of AWS)
